a44f07fe84bc011e42912e6cd6729b15e34af7b5
vyas45 <vyas45@gmail.com>
2017-03-09 14:22:23 -0800

    HashTable with Insert(), Delete() and Print()

--- Chapter1/Basics/HashTable.c
+++ Chapter1/Basics/HashTable.c
@@ -1,71 +1,120 @@
-/* Implement a basic hash table */
+/* Implement the basic Hash Table Routines */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-/* Define a node to store data in HashTable */
-struct node {
-    int data;
-    struct node *next;
+/* Define a basic node */
+struct hnode {
+  int data;
+  struct hnode* next;
 };
 
-/* The Hash Table */
+/* Define the head of the array */
+struct headList {
+  struct hnode* hList;
+};
+
+/* Define the hash table */
 struct hash {
-    struct node* arr; //Array of "node" type
-    int *num_coll; //Number of collisions
-    int size; //Size of hashtable
+  struct headList* arr; //Array of heads
+  int* num_coll; //Number of collisions
+  int size; //Size of hash table
 };
+
 
 /* Init the hash table */
-struct hash* initHash(struct hash** h, int size) {
+struct hash* hInit(int size) {
+  struct hash* h = NULL;
   int i = 0;
-  *h = malloc(sizeof(struct hash));
-  *h->arr = malloc(sizeof(struct node) * size);
-  *h->num_coll = malloc(sizeof(int) * size);
-  *h->size =size;
-  
-  /* Init all the head pointers */
+
+  h = malloc(sizeof(struct hash));
+  h->arr = malloc(sizeof(struct headList) * size); //Array of ptrs
+  h->num_coll = malloc(sizeof(int) * size);
+  h->size  = size;
+
   for(i=0; i<size; i++) {
-   *h->arr[i] = NULL;
-   *h->num_coll = -1; //Start with -1 and increment it unconditionally
+    h->arr[i].hList = NULL;
+    h->num_coll[i] = -1;//We start with -1 to "always" increment
   }
+
+  return h; //Return the hash table object
 }
 
-/* Create a new node of type "node" */
-struct node* createNode(int item) {
-  struct node* newNode = malloc(sizeof(struct node));
+/* Create a new Node */
+struct hnode* createNode(int item) {
+  struct hnode* newNode = NULL;
+
+  newNode = malloc(sizeof(struct hnode));
   newNode->data = item;
   newNode->next = NULL;
+
   return newNode;
 }
 
-/* Compute the hash for the integer */
-int computeHash(struct hash *h, int item) {
-  return( item % h->size);
+/* Compute Hash */
+int computeHash(struct hash* h, int item) {
+  return( item % (h->size));
 }
 
-/* Insert entry into the hash */
-void Insert(struct hash** h, int item) {
-    if(*h == NULL) return;
+/* Insert an element into the hash table */
+void Insert(struct hash* h, int item) {
+  if(h==NULL) return;
+  struct hnode* newNode = createNode(item);
+  int hash  = 0;
 
-    struct node* newNode =  createNode(item); //Create a new Node
-    int hash = computeHash(*h, item); //Compute the has to insert at the right position
+  /* Compute the hash for the input */
+  hash = computeHash(h, item);
 
-    newNode->next = *h->arr[hash]; //Point it to the head
-    *h->arr[hash] = newNode; //Make it the head
-    *h->num_coll++; //Increment the number of colissions
+  /* Insert at the head-end of the appropriate head */
+  newNode->next = h->arr[hash].hList;
+  h->arr[hash].hList = newNode;
+  h->num_coll[hash]++;
 }
 
-/* Printer for the hash table */
+/* Delete element from the hash table */
+void Delete(struct hash* h, int item) {
+  struct hnode* curr = NULL;
+  int hash = computeHash(h, item);
+  bool flag = false; //Keep a flag to check if we found an element
+
+  curr = h->arr[hash].hList;
+
+  /*
+   * If the element is at the head
+   * need to remove it and update the head
+   */
+   if(curr->data == item) {
+     flag = true;
+      h->arr[hash].hList = curr->next;
+      free(curr);
+   }
+   while(curr->next!=NULL) {
+    if(curr->next->data == item) {
+      flag = true;
+      struct hnode* temp = curr->next;
+      curr->next = curr->next->next;
+      free(temp);
+    }
+    curr = curr->next;
+   }
+
+   if(!flag)
+     printf("Element not found to delete!!\n");
+}
+
+/* Printer for the HashTable */
 void Printer(struct hash* h) {
-  struct node* curr = NULL;
+  printf("Printing the hash table\n\n");
+  if(h == NULL) return;
+  struct hnode* curr = NULL;
   int i = 0;
 
   for(i=0; i<h->size; i++) {
-    printf("Printing the hashTable for node #%d\n\n", i);
-    curr = h->arr[i];
+    printf("Printing the entry for index %d\n", i);
+    curr = h->arr[i].hList;
     while(curr!=NULL) {
       printf("%d--> ", curr->data);
-      curr =  curr->next;
+      curr = curr->next;
     }
     printf("\n\n");
   }
@@ -74,13 +123,20 @@ void Printer(struct hash* h) {
 int main() {
   struct hash* h = NULL;
 
-  initHash(&h, 5);
+  h = hInit(5);
 
-  Insert(&h, 1 );
-  Insert(&h, 2 );
-  Insert(&h, 3 );
+  /* Insert Data into Hash Table */
+  Insert(h, 1);
+  Insert(h, 2);
+  Insert(h, 3);
+  Insert(h, 6);
+  Insert(h, 5);
+  Insert(h, 12);
 
-  //Remove();
+  Printer(h);
+
+  /* Delete from the hash Table */
+  Delete(h, 12);
 
   Printer(h);
 
