60f48784a48da361f438c194c12fc129545b2768
vyas45 <vyas45@gmail.com>
2017-03-16 20:47:45 -0700

    Problem1. Stack doesnt work right still. Fix it in morning.

--- Chapter3/Problem1.c
+++ Chapter3/Problem1.c
@@ -97,11 +97,119 @@ bool isStackEmpty(struct Stack* stack, int id) {
  * Push into the Stack1
  */
 void Push1(struct Stack* stack, int item) {
+  if(isStackFull(stack, 1)) {
+    printf("Overflow\n");
+    return;
+  }
+  stack->top1++;
+  printf("Pushing %d into stack1 at %d\n", item, stack->top1);
+  stack->arr[stack->top1] = item;
 }
+
+
+/*
+ * Push into the Stack2
+ */
+void Push2(struct Stack* stack, int item) {
+  if(isStackFull(stack, 2)) {
+    printf("Overflow\n");
+    return;
+  }
+  stack->top2++;
+  printf("Pushing %d into stack2 at %d\n", item, stack->top2);
+  stack->arr[stack->top1] = item;
+}
+
+/*
+ * Push into the Stack3
+ */
+void Push3(struct Stack* stack, int item) {
+  if(isStackFull(stack, 3)) {
+    printf("Overflow\n");
+    return;
+  }
+  stack->top3++;
+  printf("Pushing %d into stack3 at %d\n", item, stack->top3);
+  stack->arr[stack->top3] = item;
+}
+
+/*
+ * Pop from the first stack
+ */
+void Pop1(struct Stack* stack) {
+  if(isStackEmpty(stack, 1)) {
+    printf("Underflow\n");
+    return;
+  }
+  printf("Popping %d from the stack1\n", stack->arr[stack->top1]);
+  stack->top1--;
+}
+
+/*
+ * Pop from the second stack
+ */
+void Pop2(struct Stack* stack) {
+  if(isStackEmpty(stack, 2)) {
+    printf("Underflow\n");
+    return;
+  }
+  printf("Popping %d from the stack2\n", stack->arr[stack->top2]);
+  stack->top2--;
+}
+
+/*
+ * Pop from the third stack
+ */
+void Pop3(struct Stack* stack) {
+  if(isStackEmpty(stack, 3)) {
+    printf("Underflow\n");
+    return;
+  }
+  printf("Popping %d from the stack3\n", stack->arr[stack->top3]);
+  stack->top3--;
+}
+
+/*
+ * Print the entire stack
+ */
+ void printer(struct Stack* stack) {
+   int topper = -1;
+   int i = 0;
+
+   printf("-------- Printing the stacks ---------\n");
+   printf("\n Stack1 :-> \n");
+   for(i=0; i<stack->top1; i++)
+      printf("%d\n ", stack->arr[stack->top1]);
+
+   printf("\n Stack2 :-> \n");
+   for(i=(stack->total_size/3) - 1; i<stack->top2; i++)
+     printf("%d\n ", stack->arr[stack->top2]);
+
+   printf("\n Stack3 :-> \n");
+   for(i=(2*(stack->total_size/3)) - 1; i<stack->top3; i++)
+     printf("%d\n ", stack->arr[stack->top3]);
+
+ }
 
 int main() {
   /* Init stack space of 12 units */
   struct Stack* stack = initStack(12);
+
+  Push1(stack, 1);
+  Push1(stack, 2);
+  Push1(stack, 3);
+  Push2(stack, 4);
+  Push2(stack, 5);
+  Push2(stack, 6);
+  Push3(stack, 7);
+  Push3(stack, 8);
+  Push3(stack, 9);
+
+  Pop1(stack);
+  Pop2(stack);
+  Pop3(stack);
+
+  printer(stack);
 
   return 0;
 }
