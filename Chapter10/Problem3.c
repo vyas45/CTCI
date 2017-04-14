bd17b1ad2b0b693aa255ce08135a0abab163c1c9
Aniket Vyas <vyas45@gmail.com>
2017-04-13 20:25:29 -0700

    Problem 3 solved

--- Chapter10/Problem3.c
+++ Chapter10/Problem3.c
@@ -3,29 +3,41 @@
  */
 #include <stdio.h>
 
-int findX(int arr[], int start, int end, int x) {
-  if(start<=end) {
-    int mid = (start+end)/2;
-    printf("Mid is %d and the element is %d\n", mid, arr[mid]);
-    /* try to get the flipping element */
-    if(arr[mid-1]>=arr[mid]) return mid;
-    findX(arr, start, x, mid-1);
-    findX(arr, mid+1, x, end);
-  }
-  return -1;
-}
-int main() {
-  int arr[] = {15, 16, 19, 20, 25, 28, 1};
-  int size = 0;
+int findX(int arr[], int low, int high, int x) {
+  int mid = (low+high)/2;
+  if(arr[mid] == x)
+      return mid;
+  if(low>high)
+      return -1;
 
+  /* One of the sub-arrays has to be sorted
+   * Check which one
+   */
+  /* Left is sorted */
+  if(arr[low]<arr[mid]) {
+    if(arr[low] <= x && x<= arr[mid])
+      return(findX(arr, low, mid-1, x));
+    return(findX(arr, mid+1, high, x));
+  }
+  else {
+    if(arr[mid+1]<=x && x<=arr[high])
+      return(findX(arr, mid+1, high, x));
+    return(findX(arr, low, mid-1, x));
+  }
+}
+
+int main() {
+  int arr[] = {15, 16, 19, 20, 25, 28, 1, 3, 4, 5, 7, 10, 14};
+  int size = 0;
   int x = 0;
   int index = 0;
+
   size = sizeof(arr)/sizeof(int);
 
   while(1) {
     scanf("%d", &x);
     index = findX(arr, 0, size-1, x);
-    if(index) {
+    if(index>=0) {
       printf("The index is %d\n", index);
     }
     else {
