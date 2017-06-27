b35ddfc355ba6f9ffa65b5d214c2417cb5648469
anikvyas <vyas45@gmail.com>
2017-06-26 21:05:57 -0700

    Terenary stack implementation

--- Rev2/Chapter3/Problem1.c
+++ Rev2/Chapter3/Problem1.c
@@ -15,6 +15,7 @@ struct Stack {
 /* Init the stack */
 struct Stack* stackInit(int size) {
   struct Stack* stack = malloc(sizeof(struct Stack));
+  stack->arr = calloc(size, sizeof(int));
   stack->top1 = -1;
   stack->top2 = (size/3) - 1;
   stack->top3 = (2 * (size/3)) - 1;
@@ -25,9 +26,9 @@ struct Stack* stackInit(int size) {
 /* Check if a stack is full */
 bool isStackFull(struct Stack* stack, int num) {
   switch(num) {
-    case1: return(stack->top1 == ((stack->size)/3) - 1);
-    case2: return(stack->top2 == (2 * ((stack->size)/3)) - 1);
-    case3: return(stack->top3 == ((stack->size)) - 1);
+    case 1: return(stack->top1 == ((stack->size)/3) - 1);
+    case 2: return(stack->top2 == (2 * ((stack->size)/3)) - 1);
+    case 3: return(stack->top3 == ((stack->size)) - 1);
     default: printf("Wrong stack id entered\n");
              return true;
   }
@@ -36,9 +37,9 @@ bool isStackFull(struct Stack* stack, int num) {
 /* Check if a stack is full */
 bool isStackEmpty(struct Stack* stack, int snum) {
   switch(snum) {
-    case1: return(stack->top1 == -1);
-    case2: return(stack->top2 == ((stack->size)/3) - 1);
-    case3: return(stack->top3 == (2 *((stack->size)/3)) - 1);
+    case 1: return(stack->top1 == -1);
+    case 2: return(stack->top2 == ((stack->size)/3) - 1);
+    case 3: return(stack->top3 == (2 *((stack->size)/3)) - 1);
     default: printf("Wrong stack id entered\n");
              return true;
   }
@@ -50,6 +51,7 @@ void Push(struct Stack* stack, int item, int snum) {
     printf("Stack doesn't exist\n");
     return;
   }
+  printf("\n**** Pushing %d into Stack #%d ****\n\n", item, snum);
   /* Check if the stack is full */
   if(isStackFull(stack, snum)) {
     printf("Overflow on Stack #%d\n!!", snum);
@@ -77,6 +79,7 @@ int Pop(struct Stack* stack, int snum) {
     printf("Stack doesn't exist!!\n");
     return -777;
   }
+  printf("\n**** Popping from Stack #%d ****\n\n", snum);
   if(isStackEmpty(stack, snum)) {
       printf("Stack is empty!!\n");
       return -777;
@@ -97,6 +100,23 @@ int Pop(struct Stack* stack, int snum) {
   }
 }
 
+/* Printing the stack */
+void printer(struct Stack* stack) {
+    printf("\n\n ****** Printing the stack ******\n\n");
+    int i = 0;
+    printf("\n *** STACK 1 *** \n");
+    for(i=(stack->top1); i>=0; i--)
+        printf("%d\n", stack->arr[i]);
+
+    printf("\n *** STACK 2 *** \n");
+    for(i=(stack->top2); i>=((stack->size)/3); i--)
+        printf("%d\n", stack->arr[i]);
+
+    printf("\n *** STACK 3 *** \n");
+    for(i=(stack->top3); i>=((2 * (stack->size)/3) -1 ); i--)
+        printf("%d\n", stack->arr[i]);
+}
+
 int main() {
   struct Stack* stack = NULL;
 
@@ -109,6 +129,18 @@ int main() {
   Push(stack, 3, 1); 
   Push(stack, 4, 1); 
   Push(stack, 5, 2); 
+
+  /* Pop some elements */
+  int popVal = 0;
+  popVal = Pop(stack, 1);
+  printf("Value popped is %d\n", popVal); 
+
+  popVal = Pop(stack, 3);
+  printf("Value popped is %d\n", popVal); 
+
+
+  /* Print the stack */
+  printer(stack);
 
   return 0;
 }
