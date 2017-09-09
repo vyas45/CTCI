5904ebc1b436482ac8f3a23bbff0144ca4d6e7a4
Aniket Vyas <vyas45@gmail.com>
2017-09-09 11:26:02 -0700

    isPermPalin

---
+++ Rev3/Chapter1/Problem4.c
@@ -0,0 +1,45 @@
+/*
+ * Palindrome permutation
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*
+ * Only one character can occur odd number of times
+ */
+bool isPermPalin(char* str) {
+  int i = 0;
+  int countArr[256] = {0};
+  bool flag = true;
+
+  while(*str) {
+    countArr[*str]++;
+    str++;
+  }
+
+  for(i=0; i<256; i++) {
+    if(countArr[i] %2 != 0) {
+      if(!flag)
+        return flag; //Second odd
+      if(flag) {
+        flag = false;
+      }
+    }
+  }
+  return true;//All good
+}
+
+int main() {
+  char* str = malloc(sizeof(char) * 20);
+
+  while(1) {
+    scanf("%s", str);
+    if(isPermPalin(str))
+      printf("Is a perm of palin\n");
+    else
+      printf("Not a perm of palin\n");
+  }
+
+  return 0;
+}
